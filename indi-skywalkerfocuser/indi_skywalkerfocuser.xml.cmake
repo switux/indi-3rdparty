ce53edac2c8686aa96b40eb4ac2dc7600c03242c
switux <github@datamancers.net>
2021-03-14 21:28:55 +0100

    [WIP] First beta release of the SkyWalker Focuser driver

---
+++ indi-skywalkerfocuser/indi_skywalkerfocuser.xml.cmake
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+<devGroup group="Focusers">
+<device label="SkyWalker Focuser" manufacturer="Magnetdrives">
+<driver name="SkyWalker Focuser">indi_lx200swfocuser</driver>
+    <version>@SKYWALKERFOCUSER_VERSION_MAJOR@.@SKYWALKERFOCUSER_VERSION_MINOR@</version>
+    </device>
+</devGroup>
+</driversList>
